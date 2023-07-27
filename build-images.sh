d9235cbdd58b3dcb778ad54e31ad6cad9956d128
Davide Principi <davide.principi@nethesis.it>
2023-07-27 14:39:19 +0200

    Fix GitHub Actions deprecation

--- build-images.sh
+++ build-images.sh
@@ -92,7 +92,7 @@ images+=("${repobase}/${reponame}")
 
 if [[ -n "${CI}" ]]; then
     # Set output value for Github Actions
-    printf "::set-output name=images::%s\n" "${images[*]}"
+    printf "images=%s\n" "${images[*],,}" >> "${GITHUB_OUTPUT}"
 else
     printf "Publish the images with:\n\n"
     for image in "${images[@]}"; do printf "  buildah push %s docker://%s:latest\n" "${image}" "${image}" ; done
